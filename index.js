9c3d435c221432d9a8c6dea43494e1db1aa394db
Md. Ashraful Alam <webashrafians@gmail.com>
2020-10-06 15:03:46 +0600

    volunteer network server

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Database Connection (Mongodb)
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.g7kps.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+// Add to Database ............
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const volunteersCollection = client.db("volunteer-network").collection("volunteer");
+    const volunteerTasks = client.db("volunteer-network").collection("events");
+
+  //addToDatabase.....
+  app.post("/addToDatabase", (req, res) => {
+    const fields = req.body;
+    fieldsCollection.insertMany(fields).then(result => {
+      res.send(result);
+      console.log(result.insertedCount);
+    });
+  });
+
+  //createNewEvent
+  app.post("/createNewEvent", (req, res) => {
+    const field = req.body;
+    fieldsCollection.insertOne(field).then(result => {
+      res.send(result);
+      console.log(result.insertedCount);
+    });
+  });
+
+  //eventFields
+  app.get("/eventFields", (req, res) => {
+    fieldsCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+});
+
+// events add to mongo ...................
+MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, client) {
+  const productsCollection = client
+    .db(process.env.DB_NAME)
+    .collection(process.env.DB_COLLECTION);
+
+  //addEvents ......
+  app.post("/addEvents", (req, res) => {
+    const events = req.body;
+    productsCollection.insertOne(events).then(result => {
+      res.send(result);
+      console.log(result.insertedCount);
+    });
+  });
+
+  //getEvents......
+  app.get("/getEvents", (req, res) => {
+    productsCollection
+      .find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  //allEvents .....
+  app.get("/allEvents", (req, res) => {
+    productsCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  //delete event ....
+  app.delete("/delete/:id", (req, res) => {
+    productsCollection
+      .deleteOne({
+        _id: ObjectId(req.params.id),
+      })
+      .then(result => {
+        res.send(result);
+        console.log(result.deletedCount);
+      });
+  });
+});
+
+//port connection
+app.get("/", (req, res) => {
+  res.send("Welcome to our volunteer network");
+});
+
+app.listen(process.env.PORT || 7000);
